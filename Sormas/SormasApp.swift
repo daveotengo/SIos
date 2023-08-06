88e0cdef80b48dd04a07a1c535a5d05326e2e020
daveotengo <30934250+daveotengo@users.noreply.github.com>
2023-08-06 13:15:09 +0000

    Initial Commit

---
+++ Sormas/SormasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SormasApp.swift
+//  Sormas
+//
+//  Created by David Oteng on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SormasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
